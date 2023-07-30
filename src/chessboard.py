a3a1c5663d222f92f887cdd2ad17d22c50fa9e96
7R2N <maksymilian.l.bartosik@gmail.com>
2023-07-30 17:22:19 +0200

    TODO

---
+++ src/chessboard.py
@@ -0,0 +1,99 @@
+from colorama import Back, Style
+import string
+class ChessBoard:
+    pos = [["r", "n", "b", "q", "k", "b", "n", "r"],
+           ["p", "p", "p", "p", "p", "p", "p", "p"],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", ' ', " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           ["P", "P", "P", "P", "P", "P", "P", "P"],
+           ["R", "N", "B", "Q", "K", "B", "N", "R"]]
+
+    def print(self):
+        for i in range(8):
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    print(Back.LIGHTWHITE_EX + '\033[1m' + f' {self.pos[i][j]} ' + '\033[0m' + Back.RESET, end='')
+
+                else:
+                    print(Back.BLACK + f' {self.pos[i][j]} ' + Back.RESET, end='')
+
+            print(Back.BLACK + "" + Back.RESET + f" {8 - i}")
+
+        x = 0
+        for letter in string.ascii_uppercase:
+            print(" " + letter, end=" ")
+            x += 1
+            if x == 8:
+                break
+        print("")
+
+    def turn(self):
+        piece = input("Which piece you want to move (P if you want to move pawn): \n")
+        if piece == "K":
+            pass
+        elif piece == "P":
+            pass
+        elif piece == "Q":
+            pass
+        elif piece == "R":
+            pass
+        elif piece == "N":
+            pass
+        elif piece == "B":
+            pass
+
+        # print possible moves there
+
+        # specify which piece you should move if there are two possibilities
+
+        move = list(input("Make your move: \n"))
+
+    def king_list_moves(self, piece_pos):
+        legal_moves = []
+        if 7 >= piece_pos[0] >= 0:
+            if 7 >= piece_pos[1] >= 0:
+                # |X|0|X|
+                # |X|K|X|
+                # |X|X|X|
+                if piece_pos[0] - 1 >= 0:
+                    legal_moves.apped((piece_pos[0] - 1, piece_pos[1]))
+                # |X|X|X|
+                # |X|K|X|
+                # |X|0|X|
+                if piece_pos[0] + 1 <= 7:
+                    legal_moves.apped((piece_pos[0] + 1, piece_pos[1]))
+                # |X|X|X|
+                # |0|K|X|
+                # |X|X|X|
+                if piece_pos[1] - 1 >= 0:
+                    legal_moves.apped((piece_pos[0], piece_pos[1] - 1))
+                # |X|X|X|
+                # |X|K|0|
+                # |X|X|X|
+                if piece_pos[1] + 1 <= 7:
+                    legal_moves.apped((piece_pos[0], piece_pos[1] + 1))
+                # |0|X|X|
+                # |X|K|X|
+                # |X|X|X|
+                if piece_pos[0] - 1 >= 0 and piece_pos[1] - 1 >= 0:
+                    legal_moves.apped((piece_pos[0] - 1, piece_pos[1] - 1))
+                # |X|X|0|
+                # |X|K|X|
+                # |X|X|X|
+                if piece_pos[0] - 1 >= 0 and piece_pos[1] + 1 <= 7:
+                    legal_moves.apped((piece_pos[0] - 1, piece_pos[1] - 1))
+                # |X|X|X|
+                # |X|K|X|
+                # |0|X|X|
+                if piece_pos[0] + 1 <= 7 and piece_pos[1] - 1 >= 0:
+                    legal_moves.apped((piece_pos[0] - 1, piece_pos[1] - 1))
+                # |X|X|X|
+                # |X|K|X|
+                # |X|X|0|
+                if piece_pos[0] + 1 <= 7 and piece_pos[1] + 1 <= 7:
+                    legal_moves.apped((piece_pos[0] - 1, piece_pos[1] - 1))
+
+    def pawn_list_moves(self, piece_pos):
+        legal_moves = []
---
+++ src/pieces.py
@@ -0,0 +1,19 @@
+class King:
+    pass
+
+
+class Pawn:
+    pass
+
+
+class Knight:
+    pass
+
+class Bishop:
+    pass
+
+class Rook:
+    pass
+
+class Queen:
+    pass
